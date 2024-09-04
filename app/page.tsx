3bc89f57613ab7d2559693ca2b38b3aade04ccd1
jamelouis <jamelouis2015@gmail.com>
2024-09-04 10:49:47 +0800

    adapt to mobile

--- app/page.tsx
+++ app/page.tsx
@@ -15,17 +15,17 @@ interface Tool {
 import category_tools from "@/data/category_tools.json";
 import toolsData from '../data/tools.json';
 
-const CategoryTool = ({icon, title, category, selected_category, onClicked} : 
-  { 
-    icon: string, 
-    title: string, 
+const CategoryTool = ({ icon, title, category, selected_category, onClicked }:
+  {
+    icon: string,
+    title: string,
     category: string,
     selected_category: string,
-    onClicked: (category: string) => {} 
+    onClicked: (category: string) => {}
   }) => {
   return (
     <li className="ml-5 mr-3 ">
-      <button 
+      <button
         className={`text-primary flex items-center w-full h-10 hover:bg-gray-300 transition-colors duration-200 ${category !== selected_category ? "" : "bg-gray-300 border-l-4 border-primary"}`}
         onClick={() => onClicked(category)}
       >
@@ -40,37 +40,71 @@ export default function Home() {
   const [tools, setTools] = useState<Tool[]>([]);
 
   const [selectedCategory, setSelectedCategory] = useState('all');
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   useEffect(() => {
     setTools(toolsData as Tool[]);
   }, []);
 
- const filterTools = selectedCategory === 'all' ? tools :
-                     tools.filter(({category}) => category.includes(selectedCategory)); 
+  const filterTools = selectedCategory === 'all' ? tools :
+    tools.filter(({ category }) => category.includes(selectedCategory));
 
+  const getCategoryTitle = (category: string) => {
+    const category_tool = category_tools.find(ct=> ct.category === category);
+    return category_tool ? category_tool.title: '未知类别';
+  };
+
+  const title = getCategoryTitle(selectedCategory);
 
   return (
     <>
-      <header className="fixed h-screen w-[240px] bg-background">
-        <div className="flex mt-[40px] ml-[30px] pb-[30px] items-center"> {/* Added items-center */}
-          <img src="investment.png" alt="logo" className="w-[30px] h-[30px] mr-[23px]"></img>
-          <div>
-            <h2 className="text-primary text-lg">投资101</h2>
-            <p className="text-secondary text-sm">在线工具箱平台</p>
+      <header className="bg-background">
+        <div className="flex justify-start pt-9 pb-4 items-center z-10 md:hidden">
+          <button onClick={() => setIsMenuOpen(true)}>
+            <img src="menu.png" alt="menu" className="w-5 h-5 mx-6"></img>
+          </button>
+          <div className="flex items-center">
+            <img src="investment.png" alt="logo" className="w-[30px] h-[30px] mr-3"></img>
+            <div>
+              <h2 className="text-primary text-lg">投资101</h2>
+              <p className="text-secondary text-sm">在线工具箱平台</p>
+            </div>
           </div>
         </div>
-        <nav className="text-white">
-          <ul className="flex flex-col gap-6">
-            {category_tools.map(({ icon, title, category}) => {
-              return <CategoryTool key={category} icon={icon} title={title} category={category} selected_category={selectedCategory} onClicked={(category: string) => setSelectedCategory(category)} />
-            })}
-          </ul>
-        </nav>
+        <div className={`fixed z-50 top-0 left-0 h-screen w-[240px] bg-background md:block ${(isMenuOpen? "block" : "hidden")}`}>
+          <div className="flex mt-[40px] ml-[30px] pb-[30px] items-center"> {/* Added items-center */}
+            <img src="investment.png" alt="logo" className="w-[30px] h-[30px] mr-[23px]"></img>
+            <div>
+              <h2 className="text-primary text-lg">投资101</h2>
+              <p className="text-secondary text-sm">在线工具箱平台</p>
+            </div>
+          </div>
+          <nav className="text-white">
+            <ul className="flex flex-col gap-6">
+              {category_tools.map(({ icon, title, category }) => {
+                return <CategoryTool key={category} icon={icon} title={title} category={category} selected_category={selectedCategory} onClicked={
+                  (category: string) => {
+                    setSelectedCategory(category) 
+                    if(isMenuOpen) setIsMenuOpen(false);
+                  }
+                }
+                  />
+              })}
+            </ul>
+          </nav>
+        </div>
+        { isMenuOpen && 
+          <div className="fixed top-0 left-0 w-screen h-screen bg-gray-500 opacity-10 z-30" onClick={() => setIsMenuOpen(false)}></div>
+        }
       </header>
-      <main className="pl-[240px]">
+      <main className="md:pl-[240px]">
         <div className="min-h-screen bg-background-light">
           <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-center text-gray-900 mb-12">工具箱</h1>
+            <h1 className="text-3xl font-bold text-center text-gray-900 mb-12">{title}</h1>
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
               {filterTools.map((tool) => (
                 <Link href={`/tool/${tool.id}`} key={tool.id}>

23bd412cfe182855e7f48fdc2c4b714687885f9b
jamelouis <jamelouis2015@gmail.com>
2024-09-13 15:50:09 +0800

    update run_script.yml

--- script/main.py
+++ script/main.py
@@ -1,6 +1,11 @@
 import akshare as ak
 import os
 
+data_folder = 'public/csv'
+
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
+
 symbol="sz399001"
 stock_zh_index_daily_df = ak.stock_zh_index_daily(symbol)
-stock_zh_index_daily_df.to_csv(f'public/csv/{symbol}.csv')
+stock_zh_index_daily_df.to_csv(f'{data_folder}/{symbol}.csv')
